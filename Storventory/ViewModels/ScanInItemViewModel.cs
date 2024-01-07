ab6864be561ac92b2246f1ba423be638d1ce584b
NadaHacker <nadavague@gmail.com>
2024-01-06 20:13:32 -0600

    idk what I did

--- Storventory/ViewModels/ScanInItemViewModel.cs
+++ Storventory/ViewModels/ScanInItemViewModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Storventory.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Storventory.ViewModels
 {
-    public class ScanInItemViewModel
+    public class ScanInItemViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<InventoryItem> _scannedInItems;
+        public ObservableCollection<InventoryItem> ScannedInItems
+        {
+            get => _scannedInItems;
+            set
+            {
+                _scannedInItems = value;
+                OnPropertyChanged(nameof(ScannedInItems));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
