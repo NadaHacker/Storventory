b55f767760f4d7073fa75abda8eccef115f4c093
NadaHacker <nadavague@gmail.com>
2024-01-06 19:51:22 -0600

    initial commit

---
+++ Storventory/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Storventory.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storventory.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private object _currentPage;
+
+        public MainWindowViewModel()
+        {
+            CurrentPage = new ScanInItemView();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public object CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+            }
+        }
+    }
+}
